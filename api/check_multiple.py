558e8812b36ea8207d27a170323eb33270908301
Wolfgang Maehr <wm@njyo.net>
2026-03-03 03:14:50 +0100

    Code fix

--- api/check_multiple.py
+++ api/check_multiple.py
@@ -5,79 +5,66 @@ import requests
 import os
 import json
 
-# Function to check if a given certificate code is valid
+# Function to check if a comma-separated list of certificate codes are valid
 # Parameters:
 # 	MANDATORY
-# 		?code -> the certificate code to be checked
-# 	OPTIONAL
-#		?expiry -> if provided, returns TRUE or FALSE, if the certificate is valid on a given date
-# 		?email -> if provided, returns TRUE or FALSE, if the certificate belongs to the given email
+# 		?codes -> the list of certificate codes to be checked
+#		?expiry -> returns the certificate codes that are valid and invalid
 
 class handler(BaseHTTPRequestHandler):
 
 	def do_GET(self):
 		try: 
-			# Parse input parameters /?code=7728373450WM
+			# Parse input parameters /?codes=7728373450WM,123456789XY
 			url_path = urlparse(self.path)
 			url_params = parse_qs(url_path.query)
-			cert_code = url_params.get('code', [''])[0]
+			cert_codes = url_params.get('codes', [''])[0].split(',')
 			cert_expiry = url_params.get('expiry', [''])[0]
-			cert_email = url_params.get('email', [''])[0]
 		
 			# Check that cert_code is provided
-			if not cert_code:
+			if cert_code.len() == 0 or not cert_expiry):
 				self.send_response(400)
 				self.send_header('Content-type', 'text/plain')
 				self.end_headers()
-				self.wfile.write(f'Missing "code" query parameter in path: {url_path} and params: {url_params}')
+				self.wfile.write(f'Missing "code" or "expiry" query parameter in path: {url_path} and params: {url_params}')
 				return
-			
+
+			# Create answer object
+			ans = json.loads('{"valid":[],"invalid":[]}')
+
 			# Create new request URL with code and ws_token
 			ws_token = os.environ.get('ws_token', '')
-			new_url = f"https://academy.wfdf.sport/webservice/rest/server.php?wstoken={ws_token}&wsfunction=local_validatecert_validate_certificate&moodlewsrestformat=json&code={cert_code}"
+			for cert_code in cert_codes:
+				new_url = f"https://academy.wfdf.sport/webservice/rest/server.php?wstoken={ws_token}&wsfunction=local_validatecert_validate_certificate&moodlewsrestformat=json&code={cert_code}"
 			
-			# Call the wrapped API
-			response = requests.get(new_url,timeout=10)
+				# Call the wrapped API
+				response = requests.get(new_url,timeout=10)
 			
-			# Return error, if we don't receive 200–299 status code
-			if 200 > response.status_code or 299 < response.status_code: 
-				self.send_response(response.status_code)
-				self.send_header('Content-type', 'text/plain')
-				self.end_headers()
-				self.wfile.write(f"Error: {str(e)}".encode('utf-8'))
-				return
-			
-			res_obj = json.loads(response.content.decode('UTF-8'))
-
-			# Return the response
-			self.send_response(response.status_code)
-			self.send_header('Content-type', 'text/plain')
-			self.end_headers()
-			
-			# if the certificate code is false
-			if res_obj['valid'] == False:
-				self.wfile.write(str(False).encode('utf-8'))
-				return
-
-			# If cert_expiry or cert_email is set, return TRUE or FALSE			
-			if cert_expiry or cert_email:
-				res_email = res_obj['email']
+				# Return error, if we don't receive 200–299 status code
+				if 200 > response.status_code or 299 < response.status_code: 
+					self.send_response(response.status_code)
+					self.send_header('Content-type', 'text/plain')
+					self.end_headers()
+					self.wfile.write(f"Error: {str(e)}".encode('utf-8'))
+					return
+				
+				# Parse the expiry date
+				res_obj = json.loads(response.content.decode('UTF-8'))
+				res_valid = res_obj['valid']
 				res_expiry = "9999-12-31"	# mapping over NEVER
 				if res_obj['expires'] != "NEVER":
 					res_expiry = res_obj['expires']
-				if cert_expiry and cert_email:
-					self.wfile.write(str(cert_expiry <= res_expiry and cert_email == res_email).encode('utf-8'))
-					return
-				if cert_expiry:
-					
-					self.wfile.write(str(cert_expiry <= res_expiry).encode('utf-8'))
-					return
-				else: 
-					self.wfile.write(str(cert_email == res_email).encode('utf-8'))
-					return
-			
-			# Else return the full response
-			self.wfile.write(response.content)
+
+				# Compare the date and validity and add to the correct array
+				if res_valid and cert_expiry <= res_expiry:
+					ans['valid'].append(cert_code)
+				else:
+					ans['invalid'].append(cert_code)
+
+			# Return the full response object
+			self.send_header('Content-type', response.headers.get('Content-Type', 'application/json'))
+			self.end_headers()
+			self.wfile.write(ans)
 			return
 			
 		except Exception as e:
