39b26dc9cdd3b21941050bec70dcd3fa1ce9a887
Wolfgang Maehr <wm@njyo.net>
2026-03-03 01:32:49 +0100

    Added expiry and email parameters

---
+++ api/check.py
@@ -0,0 +1,68 @@
+from http.server import BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
+import urllib.request
+import requests
+import os
+
+# Function to check if a given certificate code is valid
+# Parameters:
+# 	MANDATORY
+# 		?code -> the certificate code to be checked
+# 	OPTIONAL
+#		?expiry -> if provided, returns TRUE or FALSE, if the certificate is valid on a given date
+# 		?email -> if provided, returns TRUE or FALSE, if the certificate belongs to the given email
+
+class handler(BaseHTTPRequestHandler):
+
+	def do_GET(self):
+		try: 
+			# Parse input parameters /?code=7728373450WM
+			url_path = urlparse(self.path)
+			url_params = parse_qs(url_path.query)
+			cert_code = url_params.get('code', [''])[0]
+			cert_expiry = url_params.get('expiry', [''])[0]
+			cert_email = url_params.get('email', [''])[0]
+		
+			# Check that cert_code is provided
+			if not cert_code:
+				self.send_response(400)
+				self.send_header('Content-type', 'text/plain')
+				self.end_headers()
+				self.wfile.write(f'Missing "code" query parameter in path: {url_path} and params: {url_params}')
+				return
+			
+			# Create new request URL with code and ws_token
+			ws_token = os.environ.get('ws_token', '')
+			#ws_token = "06c3dccfbd90b558fe1e780ded1454ac"
+			new_url = f"https://academy.wfdf.sport/webservice/rest/server.php?wstoken={ws_token}&wsfunction=local_validatecert_validate_certificate&moodlewsrestformat=json&code={cert_code}"
+			
+			# Call the wrapped API
+			response = requests.get(new_url,headers={"Authorization": f"Bearer {ws_token}"},timeout=10)
+			
+			# Return error, if we don't receive 200–299 status code
+			if 200 > response.code or 299 < response.code: 
+				self.send_response(response.status_code)
+				self.send_header('Content-type', 'text/plain')
+				self.end_headers()
+				self.wfile.write(f"Error: {str(e)}".encode('utf-8'))
+				return
+			
+			# If cert_expiry or cert_email is set, return TRUE or FALSE
+			if cert_expiry or cert_email:
+				self.send_response(response.status_code)
+				self.send_header('Content-type', 'text/plain')
+				self.end_headers()
+				self.wfile.write(cert_expiry <= response.content.get('expiry') and cert_email == response.content.get('email'))
+				return
+			
+			# Return the response
+			self.send_response(response.status_code)
+			self.send_header('Content-type', response.headers.get('Content-Type', 'application/json'))
+			self.end_headers()
+			self.wfile.write(response.content)
+			
+		except Exception as e:
+			self.send_response(500)
+			self.send_header('Content-type', 'text/plain')
+			self.end_headers()
+			self.wfile.write(f"Error: {str(e)}".encode('utf-8'))
